f501ff569b5113f4f27e73919c74261481bc2c9f
bluuweb <contacto@bluuweb.cl>
2023-04-22 16:47:37 -0400

    dia 2 ❤

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Url;
+use App\Rules\UniqueUserUrl;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Validation\ValidationException;
 
 class UrlController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
-        //
+        return Inertia::render('Dashboard');
     }
 
     /**
@@ -26,9 +31,28 @@ class UrlController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+
+        // dd($request->all());
+        $validated = $request->validate([
+            'url' => ['required', 'url', new UniqueUserUrl],
+        ]);
+
+        // revisar si la url ya existe
+        // $url = $request->user()->urls()->firstWhere('url', $validated['url']);
+
+        // if ($url) {
+        //     throw ValidationException::withMessages(['url' => 'URL already exists.']);
+        // }
+
+        // generando el short_url
+        $validated['short_url'] = substr(md5($request->user()->id . $validated['url']), 0, 5);
+
+        // guardar la url en la base de datos
+        $request->user()->urls()->create($validated);
+
+        return redirect()->route('dashboard');
     }
 
     /**
