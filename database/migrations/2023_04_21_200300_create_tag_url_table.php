27ed5ba462738213982b5d1e61f35bc36e34b3c1
bluuweb <contacto@bluuweb.cl>
2023-04-22 09:56:36 -0400

    delete package

--- database/migrations/2023_04_21_200300_create_tag_url_table.php
+++ database/migrations/2023_04_21_200300_create_tag_url_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use SebastianBergmann\CodeCoverage\Report\Xml\Unit;
 
 return new class extends Migration
 {
